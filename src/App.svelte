836d13ab1a97b7e7e342f0706ae475b26c7471f7
Henri Binsztok <808274+hbbio@users.noreply.github.com>
2020-11-19 17:09:13 +0000

    css: columns

--- src/App.svelte
+++ src/App.svelte
@@ -41,12 +41,53 @@
   };
 </script>
 
-{#each Object.entries($items) as [id, md] (id)}
-  <EditRich {id} {md} {td} on:input={(ev) => updateItem(id, ev.detail)} />
-{/each}
+<style>
+  .wrapper {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-auto-rows: 100px;
+  }
 
-<ol>
-  {#each Object.entries($items) as [id, md] (id)}
-    <EditMark {id} {md} on:input={(ev) => updateItem(id, ev.detail)} />
-  {/each}
-</ol>
+  .box1 {
+    grid-column-start: 1;
+    grid-column-end: 2;
+    grid-row-start: 1;
+    grid-row-end: 3;
+  }
+
+  .box2 {
+    grid-column-start: 2;
+    grid-row-start: 3;
+    grid-row-end: 5;
+  }
+
+  .box3 {
+    grid-column-start: 3;
+    grid-row-start: 3;
+    grid-row-end: 5;
+  }
+</style>
+
+<div class="wrapper">
+  <div class="box1">
+    {#each Object.entries($items) as [id, md] (id)}
+      <EditRich {id} {md} {td} on:input={(ev) => updateItem(id, ev.detail)} />
+    {/each}
+  </div>
+
+  <div class="box2">
+    <ol>
+      {#each Object.entries($items) as [id, md] (id)}
+        <EditMark {id} {md} on:input={(ev) => updateItem(id, ev.detail)} />
+      {/each}
+    </ol>
+  </div>
+
+  <div class="box3">
+    <ol>
+      {#each Object.entries($items) as [id, md] (id)}
+        <EditMark {id} {md} on:input={(ev) => updateItem(id, ev.detail)} />
+      {/each}
+    </ol>
+  </div>
+</div>
